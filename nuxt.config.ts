6f4727dbe1e56196ca02de9985852bc26614b23f
Ilya Belyansky <win7user10@yandex.ru>
2026-03-16 10:01:58 +0300

    rebuild app with new base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
       ],
       viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
     },
-    baseURL: '/note-board-app/'
+    baseURL: '/msgboard-app/'
   },
   runtimeConfig: {
     public: {

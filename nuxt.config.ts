7a4151e07a61af4b457c2328eb0a6572e6540559
Ilya Belyansky <win7user10@yandex.ru>
2026-03-21 15:32:56 +0300

    fix absolute path address

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      messagesBaseAddress: process.env.NUXT_PUBLIC_MESSAGES_BASE_ADDRESS || 'https://laraue.com/api/notes-board',
+      messagesBaseAddress: process.env.NUXT_PUBLIC_MESSAGES_BASE_ADDRESS || 'https://laraue.com/api/notes-board/',
       testUserToken: process.env.NUXT_PUBLIC_TEST_USER_TOKEN,
     },
   },
